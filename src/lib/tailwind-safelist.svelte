249fbeccb00d7f3fed9539e9d6de4ece8ccbf616
chaeu <chaeu.email@gmail.com>
2025-11-25 17:19:05 +0100

    Fix Newsletter error

--- src/lib/tailwind-safelist.svelte
+++ src/lib/tailwind-safelist.svelte
@@ -1,5 +1,5 @@
 <!-- Auto-generated Tailwind safelist - DO NOT EDIT -->
-<!-- Generated: 2025-11-25T16:03:42.952Z -->
+<!-- Generated: 2025-11-25T16:16:41.288Z -->
 <!-- Classes: bg-primary bg-secondary bg-light-white text-white text-slate-900 text-slate-800 -->
 
 <script>
